e0697b222e88415989c93a241ab1915f343a628f
Priya Ranjan Singh <mail@priyaranjan.net>
2016-05-30 14:55:15 +0530

    add superagent to make api request

--- src/routes/Gallery/containers/GalleryContainer.js
+++ src/routes/Gallery/containers/GalleryContainer.js
@@ -1,4 +1,5 @@
 import { connect } from 'react-redux'
+const request = require('superagent')
 
 /*  This is a container component. Notice it does not contain any JSX,
     nor does it import React. This component is **only** responsible for
@@ -30,5 +31,19 @@ const mapStateToProps = (state) => ({
     Selectors are efficient. A selector is not recomputed unless one of its arguments change.
     Selectors are composable. They can be used as input to other selectors.
     https://github.com/reactjs/reselect    */
+
+const UNSPLASH_CLIENT_ID = '8f113d7ffc06c7a8f4c8a397dd5b1fd01d884211225ba623ca4a9d4c96097572'
+const SEARCH_QUERY = 'paris'
+
+request
+.get('https://api.unsplash.com/photos/search/')
+.query({
+  'client_id': UNSPLASH_CLIENT_ID,
+  'query': SEARCH_QUERY
+})
+.end((err, res) => {
+  console.log('error is: ', err)
+  console.log('response is: ', res && res.body)
+})
 
 export default connect(mapStateToProps, mapActionCreators)(Gallery)
