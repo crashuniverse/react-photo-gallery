e75e61a51ea80f73c8579729fca335e30e341c80
Priya Ranjan Singh <mail@priyaranjan.net>
2016-06-02 15:31:46 +0530

    make gallery default route and remove navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,9 @@
 import React from 'react'
-import { IndexLink, Link } from 'react-router'
 import classes from './Header.scss'
 
 export const Header = () => (
   <div>
-    <h1>React Redux Starter Kit</h1>
-    <IndexLink to='/' activeClassName={classes.activeRoute}>
-      Home
-    </IndexLink>
-    {' · '}
-    <Link to='/counter' activeClassName={classes.activeRoute}>
-      Counter
-    </Link>
-    {' · '}
-    <Link to='/gallery' activeClassName={classes.activeRoute}>
-      Gallery
-    </Link>
+    <h3 className={classes.header}>Photo Gallery</h3>
   </div>
 )
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 // We only need to import the modules necessary for initial render
 import CoreLayout from '../layouts/CoreLayout/CoreLayout'
-import Home from './Home'
 import CounterRoute from './Counter'
 import GalleryRoute from './Gallery'
 
@@ -10,10 +9,9 @@ import GalleryRoute from './Gallery'
 export const createRoutes = (store) => ({
   path: '/',
   component: CoreLayout,
-  indexRoute: Home,
+  indexRoute: GalleryRoute(store),
   childRoutes: [
-    CounterRoute(store),
-    GalleryRoute(store)
+    CounterRoute(store)
   ]
 })
 
