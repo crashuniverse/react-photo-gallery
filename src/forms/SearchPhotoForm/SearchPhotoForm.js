b63b0d7dc17858f6b2b26c61b8af97cb56f98c36
Priya Ranjan Singh <mail@priyaranjan.net>
2016-06-02 15:39:50 +0530

    add markup and styles for search box

--- src/forms/SearchPhotoForm/SearchPhotoForm.js
+++ src/forms/SearchPhotoForm/SearchPhotoForm.js
@@ -23,9 +23,16 @@ export class SearchPhoto extends React.Component {
     const { fields, handleSubmit } = this.props
 
     return (
-      <form onSubmit={handleSubmit}>
-        <input type='text' placeholder='Search Photos' autoComplete='off' required {...fields.photoSearchQuery} />
-        <button type='submit' onClick={handleSubmit}>Search</button>
+      <form className='row' onSubmit={handleSubmit}>
+        <div className='col-xs-12 col-md-6 col-md-offset-3'>
+          <div className='input-group'>
+            <input type='text' className='form-control' placeholder='Search Photos' autoComplete='off' required
+              {...fields.photoSearchQuery} />
+            <span className='input-group-btn'>
+              <button className='btn btn-default' type='button' onClick={handleSubmit}>Search</button>
+            </span>
+          </div>
+        </div>
       </form>
     )
   }
